9600f97d055d01ff11a4169e9815e167af91d0a0
rahul93anand <rahul93anand@gmail.com>
2015-10-31 19:37:59 +0530

    First push

--- projectbasedlearning/settings.py
+++ projectbasedlearning/settings.py
@@ -56,6 +56,7 @@ ROOT_URLCONF = 'projectbasedlearning.urls'
 
 TEMPLATES = [
     {
+        
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [os.path.join(BASE_DIR,"templates")],
         'APP_DIRS': True,
